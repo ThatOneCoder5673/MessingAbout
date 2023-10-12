4365898cf32c214e4400fd168ccce340d9bff00d
ThatOneCoder5673 <144918406+ThatOneCoder5673@users.noreply.github.com>
2023-10-12 19:18:18 +0000

    Adding Hooks

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div id="root">
-    <MainContent />
+    <NavBars />
+    <FavoriteColor />
     </div>
     );
 }
-  function MainContent() {
+  function NavBars() {
     return(
       <div id="MainCon">
     <div className="graybar">
     <div className="row">
     <a className="hoverpink" style= {{ textAlign: "center"}} href=" ">FREE 1-3 Day Shipping Over $79</a>
-    <a className="sidegray hovergreen clickaqua" href ="https://www.gamestop.com/stores/?showMap=true&horizontalView=true&isForm=true"><i className="material-icons sidegrayicon">storefront</i>{"\n"}Your Store</a>
-    <a className="sidegray clicklime" href= "https://www.gamestop.com/orders/"><i className="material-icons sidegrayicon">assignment</i>{"\n"}Track Order</a>
+    <a style= {{ Display: "inline-flex", texAlign: "right" }} className="sidegray hovergreen clickaqua" href ="https://www.gamestop.com/stores/?showMap=true&horizontalView=true&isForm=true"><i className="material-icons sidegrayicon">storefront</i>{"\n"}Your Store</a>
+    <a style= {{ Display: "inline-flex", texAlign: "right" }} className="sidegray clicklime" href= "https://www.gamestop.com/orders/"><i className="material-icons sidegrayicon">assignment</i>{"\n"}Track Order</a>
     </div>
     </div>
     <div className="rainbowbar">
@@ -23,7 +25,7 @@ function App() {
     </div></div>
     <div className="gmebar">
     <div className="row">
-    <a href=" " className="inline" style= {{ FontFace: "Poppins", fontSize: "11px", textAlign: "center" }}><i style={{ textAlign: "center" }} className="material-icons">density_medium</i>Menu</a>
+    <a href=" " className="inline" style= {{ FontFace: "Poppins", fontSize: "11px", textAlign: "center", TextDecoration: "none" }}><i style={{ textAlign: "center" }} className="material-icons">density_medium</i>Menu</a>
     <a href="https://www.gamestop.com/"><img className="llogo" alt="nah" src="https://www.gamestop.com/on/demandware.static/Sites-gamestop-us-Site/-/default/dw3fe7a996/images/svg-icons/logo-gs-mono.svg"/></a>
     <div className="searcharea"><a style= {{textDecoration: 'none'}} href=" "><button className="searchbutt"><i className="material-icons">search</i></button><input className="searchbar" type="text" autocomplete="on" placeholder="Search games, consoles & more"/></a></div>
     </div>
@@ -32,5 +34,22 @@ function App() {
     </div>);
     
 }
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+  const MyArray = ["blue", "pink", "yellow"];
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor(MyArray[0])}
+      >Blue</button>
+      
+    </>
+  )
+}
+
+
 
 export default App;

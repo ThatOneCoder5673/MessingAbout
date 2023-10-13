26925a79e75bf1555c4fc90c3b2a4a86fbb01337
ThatOneCoder5673 <144918406+ThatOneCoder5673@users.noreply.github.com>
2023-10-13 18:26:57 +0000

    Done

--- messingabout/src/App.js
+++ messingabout/src/App.js
@@ -1,38 +1,40 @@
 import './App.css';
-import { useState } from "react";
-import 'myapp/public/buckthebunny.png';
+import React, { useState } from "react";
 
 function App() {
   return (
     <div id="root">
     <NavBars />
     <ClickCounter />
-    </div>
+    <BuckBunny />
+                   </div>
     );
 }
   function NavBars() {
     return(
-      <div id="MainCon">
-    <div className="graybar">
-    <div className="row">
+    <div id="MainCon">
+     <div className="graybar">
+      <div className="row">
     <a className="hoverpink" style= {{ textAlign: "center"}} href=" ">FREE 1-3 Day Shipping Over $79</a>
     <a style= {{ Display: "inline-flex", texAlign: "right" }} className="sidegray hovergreen clickaqua" href ="https://www.gamestop.com/stores/?showMap=true&horizontalView=true&isForm=true"><i className="material-icons sidegrayicon">storefront</i>{"\n"}Your Store</a>
     <a style= {{ Display: "inline-flex", texAlign: "right" }} className="sidegray clicklime" href= "https://www.gamestop.com/orders/"><i className="material-icons sidegrayicon">assignment</i>{"\n"}Track Order</a>
-    </div>
-    </div>
+      </div>
+     </div>
     <div className="rainbowbar">
-    <div className="row">
+     <div className="row">
     <a className="hoverpink clickaqua" href="https://www.gamestop.com/pro/">Get 5% Extra Off Your Purchase When You Join Or Renew GameStop Pro.*</a>
-    </div></div>
-    <div className="gmebar">
-    <div className="row">
+     </div>
+    </div>
+     <div className="gmebar">
+      <div className="row">
     <a href=" " className="inline" style= {{ FontFace: "Poppins", fontSize: "11px", textAlign: "center", TextDecoration: "none" }}><i style={{ textAlign: "center" }} className="material-icons">density_medium</i>Menu</a>
     <a href="https://www.gamestop.com/"><img className="llogo" alt="gmelogo" src="https://www.gamestop.com/on/demandware.static/Sites-gamestop-us-Site/-/default/dw3fe7a996/images/svg-icons/logo-gs-mono.svg"/></a>
-    <div className="searcharea"><a style= {{textDecoration: 'none'}} href=" "><button className="searchbutt"><i className="material-icons">search</i></button><input className="searchbar" type="text" autocomplete="on" placeholder="Search games, consoles & more"/></a></div>
+       <div className="searcharea"><button className="searchbutt"><i className="material-icons">search</i></button>
+    <input className="searchbar" type="text" autocomplete="on" placeholder="Search games, consoles & more"/>
+       </div>
+      </div>
     </div>
-        </div>
-        <img href = "myapp/public/buckthebunny.png" />
-            </div>);
+    </div>);
     
 }
 function ClickCounter() {
@@ -54,7 +56,9 @@ function ClickCounter() {
           </div>
   )
 }
-
-
+class BuckBunny extends React.Component {
+  render() {
+    return <img alt="buckbunny" src="/buckthebunny.png" style= {{width:"50px", textAlign: "center"}} /> }
+  }
 
 export default App;
